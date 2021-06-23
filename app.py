11efee3510a6f78077f4221e56a6e3ddbb42261e
ahmed-hassan97 <ahmed1997abohassan@gmail.com>
2021-06-23 21:54:57 +0200

    add project file

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+with open('gbc.pickle', 'rb') as f:
+    loaded_model = pickle.load(f)
+
+
+@app.route('/')
+def home_page():
+	return render_template('form.html')
+
+
+@app.route('/predict1' ,  methods=['POST' ,'GET'])
+def predict():
+	op1 =""
+	
+	data = [
+
+			request.form['Gender'],
+			request.form['Age'],
+			request.form['Driving_License'],
+			request.form['Region_Code'],
+			request.form['Previously_Insured'],
+			request.form['Vehicle_Age'],
+			request.form['Vehicle_Damage'],
+			request.form['Annual_Premium'],
+			request.form['Policy_Sales_Channel'],
+			request.form['Vintage']
+		]
+
+
+	data = np.array([np.asarray((data), dtype=float)])
+	predictions = loaded_model.predict(data)
+	
+	if predictions[0] == 1:
+		op1 = 'interested'
+	else:
+		op1 = 'not interested'
+	return render_template('form.html' , pred = op1)
+	
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
+
---
+++ imgs/Readme.md
@@ -0,0 +1,1 @@
+Imgs For model 
